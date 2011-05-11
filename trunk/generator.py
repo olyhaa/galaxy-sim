61d6a693b57a8747352460205cdfb0157b6462ec
inventor226 <inventor226@f3cf57ae-5208-5186-d7c4-fee286795f51>
2011-05-11 04:42:23 +0000

    Updated generator to fix bug and added movie making file

--- trunk/generator.py
+++ trunk/generator.py
@@ -7,28 +7,23 @@ import mpl_toolkits.mplot3d  as p3
 #import random
 
 
+x=[]
+y=[]
+z=[]
+xv=[]
+yv=[]
+zv=[]
+rt=[]
 
-def density(star_number):
-  """Returns how the star density varries with repect to the raduis"""
-  
-  return range(star_number)
+mass=[]
 
-#def z_location():
-  ##loc=random.randomint()
-  #return random.normal(loc=0,scale=20)
+xt=[]
+yt=[]
+valt=[]
+#r_t=arange(0,50000,10)
+#sp=[]
 
-#def speed(r):
-  #"""  """
-  ##r_scale=5
-  #r_opt=10
-  #B=50
-  #v_c=7.2
-  #if r<(.7*r_opt):
-    #x=r/r_opt
-    #v_sq=B*(1.97*x**1.22)/((x**2+0.78**2)**1.43)
-    #return v_sq**(.5)
-  #else:
-    #return .2*r+v_c
+
 
 def star_speed(r,fit):
   """  """
@@ -42,23 +37,10 @@ def star_speed(r,fit):
   
   return (fit[i+1][1]-fit[i][1])/(fit[i+1][0]-fit[i][0])*(r-fit[i][0])+fit[i][1]
 
-x=[]
-y=[]
-z=[]
-xv=[]
-yv=[]
-zv=[]
-rt=[]
-sp=[]
-mass=[]
 
-xt=[]
-yt=[]
-valt=[]
-#r_t=arange(0,50000,10)
-#sp=[]
+
+
 #Read in Speed Fit File
-
 text_file = open("MWVR_inferred.out", "r")
 fit=[]
 for line in text_file.readlines():
@@ -66,36 +48,29 @@ for line in text_file.readlines():
     #fit.append([1000*float(s) for s in line.rstrip().split()])
     temp=[float(s) for s in line.rstrip().split()]
     temp[0]=temp[0]*1000
+    temp[1]=temp[1]*0.00102268944 
+    #temp[
     fit.append(temp)
+    #print temp
+text_file.close()
 
-#for i in r_t:
-  #sp.append(speed(i,fit))
-#plt.figure()
-#plt.plot(r_t,sp)
-#text_file.close()
+#Adjust these numbers to change start density
+#Basically n is the number of ellipse to be created and m is the density of stars along those arms. 
+#Warning: If m gets to large with respect to n you must adjust the spin rate and increase n to keep it physically real
+#Warning: If n gets to large the galaxy will become to large, you must adjust scalling factor bellow
+#for about 12,000 stars use n=75 m=250                                                                                                              
+n=75.0
+m=250.0
+##Create Arms
 
-
-
-###Create Arms  
-#for i in range(n):
-  #for j in range(m):
-    #t=j+(pi/8)*random.random()
-    #X_t=(0.2*(i**2)+0.001)*sin(t)
-    #Y_t=(0.3*(i**2)+0.001)*cos(t)
-    
-    #a=(50*i)*pi/(2.0*n)
-    
-    #x.append(X_t*cos(a)-Y_t*sin(a)+100*random.random()-50)
-    #y.append(Y_t*cos(a)+X_t*sin(a)+100*random.random()-50)
-    #z.append(z_location())
-
-n=50.0
-m=100.0
 
 for i in range(int(n)):
   #print str(i)+"     "+str(int(m*exp(-2*i/n)))+"      "+str(m*exp(-2*i/n))
   for j in range(int(m*exp(-1.0*i/n))):
+    #Adjust number that multiplies i/n above to change scaling 
     #t=j#+(pi/16)*random.random()
+    
+    #Spin rates
     b=(0.2*(i**2)+0.0001)
     c=(0.4*(i**2)+0.0001)
     
@@ -111,10 +86,11 @@ for i in range(int(n)):
     y.append(32*(y_r(j)*(1+0.2*(random.random()-1))))
     
     z.append(random.normal(loc=0,scale=150))
-    rt.append((x[-1]**2+y[-1]**2)**0.5)
+    rt.append((x[-1]**2+y[-1]**2+z[-1]**2)**0.5)
+   
     speed=star_speed(rt[-1],fit)
     speed+=(speed*0.05*random.random()-speed*0.05)
-    sp.append(speed)
+    
     
     #find unit vectors for function
     #dt=j*0.002
@@ -131,25 +107,17 @@ for i in range(int(n)):
     
     xv.append(xu*speed)
     yv.append(yu*speed)
-    valt.append((xv[-1]**2+yv[-1]**2)**0.5)
-    zv.append(1)
-    
-    
+    zv.append(random.normal(scale=0.017)+0.000017*z[-1])
+    valt.append((xv[-1]**2+yv[-1]**2+zv[-1]**2)**0.5)
     #rt.append((x[-1]**2+y[-1]**2+z[-1]**2)**0.5)
+
  
-#plt.figure()
-#plt.hist(xv,100)
-#plt.title('dx')
-#plt.figure()
-#plt.hist(yv,100)
-#plt.title('dy')
-#plt.figure()
-#plt.plot(rt,valt,'.k')
-#plt.title('Val')
 
 ##Create Bulge;  30 Million Stars Compared to 200 Billion in Disk 
 n_c=len(x)*0.015
+#n_c=1000
 #print n_c
+
 for i in range(int(n_c)):
   r=4200*random.power(3)
   theta=random.random()*pi
@@ -159,29 +127,30 @@ for i in range(int(n_c)):
   x.append(r*cos(thi)*sin(theta))
   y.append(r*sin(thi)*sin(theta))
   z.append(r*cos(theta))
-  
+
   #Velocity
   speed=star_speed(r,fit)
   speed+=(speed*0.05*random.random()-speed*0.05)
-  sp.append(speed)
+
 
   rdot=speed*(0.01)*(1-random.random())
   a=2*pi*random.random()
-  thetadot=cos(a)*speed
-  thidot=sin(a)*speed
+  thetadot=cos(a)*speed/r
+  thidot=sin(a)*speed/r
   
   xv.append(cos(thi)*sin(theta)*rdot-r*sin(thi)*sin(theta)*thidot+r*cos(thi)*cos(theta)*thetadot)
   yv.append(sin(thi)*sin(theta)*rdot+r*cos(thi)*sin(theta)*thidot+r*sin(thi)*cos(theta)*thetadot)
   zv.append(cos(theta)*rdot-r*sin(theta)*thetadot)
-  
+  valt.append((xv[-1]**2+yv[-1]**2+zv[-1]**2)**0.5)
 
-if len(x)%2==1:
-  x.pop()
-  y.pop()
-  z.pop()
-  xv.pop()
-  yv.pop()
-  zv.pop()
+
+#if len(x)%2==1:
+  #x.pop()
+  #y.pop()
+  #z.pop()
+  #xv.pop()
+  #yv.pop()
+  #zv.pop()
 out=open('out.txt','w')
 out.write('%012d'%len(x)+'\n')
 for i in range(len(x)):
@@ -210,18 +179,16 @@ for i in arange(-maxval_r,maxval_r,maxval_r/(2**4)):
 
 out=open('out_dark.txt','w')
 
-for i in range(len(x)):
+for i in range(len(xdark)):
   out.write('% 018.9f'%xdark[i]+',''% 018.9f'%ydark[i]+','+'% 018.9f'%zdark[i]+'\n')
 
 out.close()
 
+#print max(rt)
+#maxval_r=maxval
 
 
 
-#print len(xdark)
-#print len(ydark)
-#print len(zdark)
-#maxval=max(x)*0.8
 fig=plt.figure() 
 ax = p3.Axes3D(fig)  
 ax.plot3D(x,y,z,'.y')
